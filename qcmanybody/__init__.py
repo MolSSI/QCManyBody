0ff157595eb62cff053d5cc33a484abb7f551f34
Benjamin Pritchard <ben@bennyp.org>
2024-03-11 16:23:33 -0400

    Export BsseEnum to top level

--- qcmanybody/__init__.py
+++ qcmanybody/__init__.py
@@ -1,6 +1,7 @@
 from importlib.metadata import version
 
 from .manybody import ManyBodyCalculator
+from .models import BsseEnum
 from .utils import labeler, delabeler
 
 __version__ = version("qcmanybody")
