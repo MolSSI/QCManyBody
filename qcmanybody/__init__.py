7c5024d2936fa69aafb7e37d5f870ec4ccfbbf3b
Lori A. Burns <lori.burns@gmail.com>
2024-05-14 00:37:46 -0400

    help docs

--- qcmanybody/__init__.py
+++ qcmanybody/__init__.py
@@ -3,5 +3,6 @@ from importlib.metadata import version
 from .manybody import ManyBodyCalculator
 from .models import BsseEnum
 from .utils import labeler, delabeler, resize_gradient, resize_hessian
+from .qcng_computer import ManyBodyComputerQCNG
 
 __version__ = version("qcmanybody")
