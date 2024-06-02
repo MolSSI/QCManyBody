01f9313b1ed5e501ff42aa9059e0d14a585d207d
Lori Burns <lori.burns@gmail.com>
2024-06-02 03:19:36 -0400

    formatting files

--- qcmanybody/__init__.py
+++ qcmanybody/__init__.py
@@ -1,8 +1,11 @@
 from importlib.metadata import version
 
+# isort: off
 from .core import ManyBodyCalculator  # legacy near-alias to ManyBodyCore
 from .core import ManyBodyCore
 from .computer import ManyBodyComputer
+
+# isort: on
 from .models import BsseEnum
 from .utils import delabeler, labeler, resize_gradient, resize_hessian
 
